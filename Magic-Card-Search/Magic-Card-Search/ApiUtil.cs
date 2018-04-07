0b8aa26ea68868497f81be4cc0922f22d2d7b821
GradyLealand <kineco@gmail.com>
2018-04-07 12:15:58 -0300

    Added class's "ApiUtil" and "CardModel"

---
+++ Magic-Card-Search/Magic-Card-Search/ApiUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MtgApiManager;
+using MtgApiManager.Lib.Service;
+
+namespace Magic_Card_Search
+{
+    class ApiUtil
+    {
+        // a list of all ccard models recived from the search
+        List<CardModel> _allCards;
+
+        //get cards based from selected set where there names contain
+        public async void GetByName(string name, string series)
+        {
+            //create card service
+            CardService service = new CardService();
+        }
+
+        //get cards form a selected set based on advanced search properties
+    }
+}
