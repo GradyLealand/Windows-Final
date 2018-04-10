389a10689ecd109409784fa72e3e5e1c7294a14b
GradyLealand <kineco@gmail.com>
2018-04-10 08:37:19 -0300

    hooked Card controller up to Card util and main page

---
+++ Magic-Card-Search/Magic-Card-Search/CardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_Card_Search
+{
+    public class CardController
+    {
+
+        private List<CardModel> _allCards = new List<CardModel>();
+
+        public CardController()
+        {
+            //testing api return
+            LoadCards();
+        }
+
+        public async void LoadCards()
+        {
+            _allCards = await CardUtil.GetCardsByName();
+        }
+    }
+}
---
+++ Magic-Card-Search/Magic-Card-Search/CardUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MtgApiManager;
+using MtgApiManager.Lib.Service;
+
+namespace Magic_Card_Search
+{
+    public class CardUtil
+    {
+        // a list of all ccard models recived from the search
+        private static List<CardModel> _allCards = new List<CardModel>();
+
+        
+        public static async void GetSet(string setCode)
+        {
+            SetService service = new SetService();
+            var result = service.Find("ktk");
+            var asyncResult = await service.FindAsync("ktk");
+        }
+        
+        public static async Task<List<CardModel>> GetCardsByName()
+        {
+            GetSet("ktk");
+            return _allCards;
+        }
+
+        //get cards form a selected set based on advanced search properties
+    }
+}
